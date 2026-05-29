5e488425ee6075db33c99eb97c6d50db90ee9292
duchesneantoine12-code <duchesneantoine12@gmail.com>
2026-05-29 13:10:30 +0200

    Add files via upload

--- assignInAll.js
+++ assignInAll.js
@@ -1,5 +1,5 @@
 var convert = require('./convert'),
-    func = convert('after', require('../after'));
+    func = convert('assignInAll', require('../assignIn'));
 
 func.placeholder = require('./placeholder');
 module.exports = func;
